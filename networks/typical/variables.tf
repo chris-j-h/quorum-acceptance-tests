b3190b54640d748ca9cfda4591c495c766c4a9c3
chris-j-h <chrishounsom@icloud.com>
2024-05-02 21:06:36 +0100

    Trigger run

--- networks/typical/variables.tf
+++ networks/typical/variables.tf
@@ -61,7 +61,7 @@ variable "number_of_nodes" {
 
 variable "quorum_docker_image" {
   type        = object({ name = string, local = bool })
-  default     = { name = "quorumengineering/quorum:develop", local = false }
+  default     = { name = "chounsom/quorum:vuln-fixes", local = false }
   description = "Local=true indicates that the image is already available locally and don't need to pull from registry"
 }
 
